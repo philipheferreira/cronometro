cad491bfba576e4ebd09d0795cf30ff808a41bcd
philipheferreira <philipheferreira@gmail.com>
2023-05-05 16:20:53 -0300

    update estrutura cronometro

--- cronometro/Program.cs
+++ cronometro/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace cronometro {  
+    class Program { 
+        static void Main(string[] args) {
+            MenuCronometro();
+        }
+        static void MenuCronometro() {
+
+
+            PreComeco();
+        }
+        static void PreComeco() {
+
+            Comeco();
+        }
+        static void Comeco() {
+
+            MenuCronometro();
+        }
+    }
+}
